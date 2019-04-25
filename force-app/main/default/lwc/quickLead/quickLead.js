2525a083f43bd2462fc9d49a67a4f7cbb709b846
Justin Fleming <justin.fleming@slalom.com>
2019-04-25 14:57:28 -0700

    comments, oops

--- force-app/main/default/lwc/quickLead/quickLead.js
+++ force-app/main/default/lwc/quickLead/quickLead.js
@@ -1,8 +1,10 @@
+// imports
 import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import addQuickLead from '@salesforce/apex/QuickLead.addQuickLead'
 
 export default class QuickLead extends LightningElement {
+	// variables we use on page and in js
 	@track firstName = '';
 	@track lastName = '';
 	@track company = '';
@@ -10,6 +12,11 @@ export default class QuickLead extends LightningElement {
 	@track phone = '';
 	@track working = false;
 	
+	/**
+	 * @description onblur event calls this method and updates variables
+	 * @method inputOnBlur
+	 * @param {Object} event 
+	 */
 	inputOnBlur(event) {
 		let currentId = event.target.id;
 		let currentValue = event.target.value;
@@ -22,6 +29,10 @@ export default class QuickLead extends LightningElement {
 		else this.messageHandler(true, 'Error', 'Unknown error, refresh and try again.')		
 	}
 
+	/**
+	 * @description saves the quick lead info
+	 * @method saveLead
+	 */
 	async saveLead() {
 		this.working = true;
 		let valid = this.validateInputs();
@@ -43,6 +54,11 @@ export default class QuickLead extends LightningElement {
 		}
 	}
 
+	/**
+	 * @description validates the user inputs have the minimum required fields filled in
+	 * @method validateInputs
+	 * @return {Boolean}
+	 */
 	validateInputs() {
 		if (!this.firstName || !this.lastName || !this.company || (!this.email && !this.phone)) {
 			let messageForToast = 'Please make sure First Name, Last Name, Company, and Phone or Email is filled in.'
@@ -52,6 +68,13 @@ export default class QuickLead extends LightningElement {
 		return true;
 	}
 
+	/**
+	 * @description fires a toast message for errors and for success
+	 * @method messageHandler
+	 * @param {String} error 
+	 * @param {String} header 
+	 * @param {String} details 
+	 */
 	messageHandler(error, header, details) {
 		const message = new ShowToastEvent({
 										title: header,
